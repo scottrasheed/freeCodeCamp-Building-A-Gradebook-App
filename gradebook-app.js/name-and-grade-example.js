47f7f1513369078f406396d913ec7718e0486d4c
Scott Rasheed <scottrasheed@ymail.com>
2025-07-21 17:37:43 -0400

    Create name-and-grade-example.js

---
+++ gradebook-app.js/name-and-grade-example.js
@@ -0,0 +1,6 @@
+const studentName = "Scottie"; // saves the name "Scottie" in a variable
+const grade = 95; // saves the number 95 in a variable
+console.log(`${studentName} got a grade of ${grade}`); // prints the message below
+
+// Output:
+// Scottie got a grade of 95
